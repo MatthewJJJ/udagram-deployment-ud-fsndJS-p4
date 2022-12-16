f59d687605e60aec1f73d878f2040afe9fe11133
MatthewJ Franklin <matthew3j02@gmail.com>
2022-12-16 10:09:21 -0600

    fixing linting errors

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
+  appName: 'Udagram',
   apiHost:
-    "http://Udagramapimjf-env.eba-nrpzsy3m.us-east-1.elasticbeanstalk.com/api/v0",
+    'http://Udagramapimjf-env.eba-nrpzsy3m.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: "Udagram",
+  appName: 'Udagram',
   apiHost:
-    "http://Udagramapimjf-env.eba-nrpzsy3m.us-east-1.elasticbeanstalk.com/api/v0",
+    'http://Udagramapimjf-env.eba-nrpzsy3m.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
